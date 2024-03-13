5babc5f95cc24b3124f46ca5e924ee9a8d642732
SanganiTushal <sanganitushal@gmail.com>
2024-03-13 09:09:16 +0530

    background changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
   return (
     <>
       {data && (
-        <footer className="py-4 bg-blue-200 mx-4">
+        <footer className="py-4 bg-amber-300 mx-4">
           <div className="flex items-center justify-center space-x-4 text-slate-500">
             {/* YouTube Channel Link */}
             <a
